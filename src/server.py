2bffd040e03cca52e4bc702110d95049a14bf6ee
a1petrov <115454268+a1petrov@users.noreply.github.com>
2025-03-15 02:26:39 +0300

    KP-4:mod server with post request

--- src/server.py
+++ src/server.py
@@ -13,20 +13,29 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data)
-        if 'name' in data:
-            response = {"message": f"Hello, {data['name']}!"}
+        expression = data.get('expression')
+        if expression is None:
+            self.send_response(500)  # Bad Request
+            self.end_headers()
+            return
+
+            # Попытка вычислить выражение
+        result = subprocess.run("./../build/app.exe",input=expression,text=True,capture_output=True)
+        if (result.returncode==0):
+            response = {"result": result.stdout}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode())
         else:
-            response = {"message": "Hello, man!"}
-        
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(response).encode())
+            response = {"result": result.stdout}
+            self.send_response(500)
+            self.end_headers()
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting httpd server on port {port}")
+    print(f"Starting http server on port {port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
